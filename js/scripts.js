e5069bb1b2e74b21652de23ff35e6d7bb4821961
Martin Cartledge and Stephen Burden <martin@>
2016-08-22 11:38:46 -0700

    add input fields for to-do-list and js to return list to user

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// back-end
+function Task (activity, deadline, location){
+  this.activity = activity;
+  this.deadline = deadline;
+  this.location = location;
+}
+
+// front-end
+$(document).ready(function(){
+  $("form#to-do").submit(function(event){
+    event.preventDefault();
+
+    var activity = $("input#activity").val();
+    var deadline = $("input#deadline").val();
+    var location = $("input#location").val();
+
+    var newTask = new Task(activity, deadline, location);
+
+    $("ul#to-do-list").append("<li><span class='task'>" + newTask.activity + "</span></li>");
+
+    $(".task").last().click(function(){
+      $("#task-details").slideDown();
+      $("h3").text(newTask.activity);
+      $(".deadline").text(newTask.deadline);
+      $(".location").text(newTask.location);
+
+      
+    });
+
+  });
+});
